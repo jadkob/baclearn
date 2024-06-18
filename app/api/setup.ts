2b4bef3b69f13d4abf5a00fce9c3cd31074f999d
jad <jadkoneissi@gmail.com>
2024-06-18 18:09:15 +0300

    connection fix

--- app/api/setup.ts
+++ app/api/setup.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://localhost:27017");
+mongoose.connect(process.env.STRING!);
 
 export const baclearn = mongoose.connection.useDb("baclearn");
